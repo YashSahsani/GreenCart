b196180205fb260c7d22950b0d71f855002a7dc5
YashSahsani <yashsahsani5@gmail.com>
2024-06-10 11:39:56 -0400

    feat: init an app

--- GreenCart/urls.py
+++ GreenCart/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+admin.site.site_header="GreenCart Admin"
+admin.site.site_title ="GreenCart Admin Portal"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('GreenCartEcom.urls'))
 ]
