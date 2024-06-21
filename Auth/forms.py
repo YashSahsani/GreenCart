0d6f969d779322064bf477d851e76fadc9d971df
YashSahsani <yashsahsani5@gmail.com>
2024-06-20 21:52:23 -0400

    feat: forgot-password

--- Auth/forms.py
+++ Auth/forms.py
@@ -29,9 +29,6 @@ class LoginForm(forms.Form):
         }),
         label='Remember me'
     )
-    class Meta:
-        model = get_user_model()
-        fields = ['email', 'password']
 
 class SignupForm(UserCreationForm):
     first_name = forms.CharField(
@@ -78,3 +75,55 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = get_user_model()
         fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
+
+
+class ForgotPasswordForm(forms.Form):
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'class': 'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm',
+            'placeholder': 'Email',
+            'id': 'email',
+            'required': True
+        }),
+        label='Email'
+    )
+
+class ResetPasswordForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        email = kwargs.pop('email', None)
+        super(ResetPasswordForm, self).__init__(*args, **kwargs)
+        if email:
+            self.fields['email'].initial = email
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'class': 'hidden',  # Hide the email input field
+            'id': 'email',
+        }),
+        label='Email'
+    )
+    password1 = forms.CharField(
+        label="New Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={
+            'class': 'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm',
+            'placeholder': 'New Password'
+        })
+    )
+    password2 = forms.CharField(
+        label="Confirm Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={
+            'class': 'mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm',
+            'placeholder': 'Confirm New Password'
+        })
+    )
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
+
+        if password1 and password2 and password1 != password2:
+            self.add_error('password2', "Passwords do not match.")
+        return cleaned_data
--- Auth/urls.py
+++ Auth/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import Signup, Login, logout_view
+from .views import Signup, Login, logout_view, forgot_password,reset_password
 app_name = 'Auth'
 
 urlpatterns = [
     path('signup/', Signup.as_view(), name='signup'),
     path('login/', Login.as_view(), name='login'),
     path('logout/', logout_view, name='logout'),
+    path('forgot_password/', forgot_password, name='forgot_password'),
+    path('reset_password/', reset_password, name='reset_password'),
 ]
