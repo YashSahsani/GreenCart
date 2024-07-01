d6b1138335484a79114876d9c0a23d0422dabbe9
yashpatel458 <yashpatel4936.yp@gmail.com>
2024-06-30 20:55:57 -0400

    Feat: Revised Products Page with elements and filter

--- Shop/views.py
+++ Shop/views.py
@@ -5,9 +5,25 @@ from .models import Product
 
 # Create your views here.
 @login_required
+
+
 def home(request):
+    query = request.GET.get('query', '')
+    min_price = request.GET.get('min_price', 0)
+    max_price = request.GET.get('max_price', 1000000)
+
     products = Product.objects.all()
-    return render(request, 'Shop/home.html',{'products': products,'title': 'GreenCart | Home','user_profile_pic': UserProfile.objects.get(user=request.user).profile_pic.url})
+
+    if query:
+        products = products.filter(name__icontains=query)
+
+    if min_price:
+        products = products.filter(price__gte=min_price)
+
+    if max_price:
+        products = products.filter(price__lte=max_price)
+
+    return render(request, 'Shop/home.html', {'products': products,'title': 'GreenCart | Home','user_profile_pic': UserProfile.objects.get(user=request.user).profile_pic.url})
 
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
