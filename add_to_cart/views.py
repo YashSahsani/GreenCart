953a96d09cddebb3faca057b19ee3a2956974b95
Param Kamdar <kamdarp@uwindsor.ca>
2024-07-08 12:25:39 -0400

    Navbar and footer added in cart page.

--- add_to_cart/views.py
+++ add_to_cart/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.views import View
 
 
 # Create your views here.
 def cart(request):
-    return render(request, 'Cart/base.html')
+    return render(request, 'Cart/cart.html')
