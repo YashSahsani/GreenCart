77117b6f96f618992158206d52d634ea2944787f
yashpatel458 <yashpatel4936.yp@gmail.com>
2024-07-09 21:47:57 -0400

    Added guides and legal information

--- Shop/urls.py
+++ Shop/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import about, home, product_detail, product_list
+from .views import about, home, privacy_policy, product_detail, product_list, terms_and_conditions, plant_care_tips, gardening_guides
 app_name = 'Shop'
 
 urlpatterns = [
@@ -11,5 +11,9 @@ urlpatterns = [
     path('product-detail/<int:id>', product_detail, name='product_detail'),
     path('mylistings/', product_list, name='product_list'),
     path('about/', about, name='about'),
+    path('privacy-policy/', privacy_policy, name='privacy_policy'),
+    path('terms-and-conditions/', terms_and_conditions, name='terms_and_conditions'),
+    path('gardening-guides/', gardening_guides, name='gardening_guides'),
+    path('plant-care-tips/', plant_care_tips, name='plant_care_tips'),
 ]
 
