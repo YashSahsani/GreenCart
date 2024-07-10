d9e696ebf9ff32a4b31d9d4e9c18ad44cbcf3bcc
Param Kamdar <kamdarp@uwindsor.ca>
2024-07-09 21:50:22 -0400

    Cart view, model and urls logic has been added.

--- add_to_cart/urls.py
+++ add_to_cart/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'add_to_cart'
 
 urlpatterns = [
-    path('', views.cart, name='cart'),
+    path('', views.cart_view, name='cart'),
+    path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove/<int:cart_item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('increment/<int:cart_item_id>/', views.increment_quantity, name='increment_quantity'),
+    path('decrement/<int:cart_item_id>/', views.decrement_quantity, name='decrement_quantity'),
 ]
