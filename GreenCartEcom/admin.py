3ffdbd8321b124797ca53156d642850fcc314a5c
Hetvi2010 <hp712793@gmail.com>
2024-07-10 20:00:31 -0400

    added offersList on LandingPage

--- GreenCartEcom/admin.py
+++ GreenCartEcom/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import navCategories
+from .models import navCategories, offer
 
 admin.site.register(navCategories)
+admin.site.register(offer)
--- GreenCartEcom/views.py
+++ GreenCartEcom/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
-from .models import navCategories
+from .models import navCategories, offer
 from Shop.models import Product
 
 def index(request):
     categoryList = navCategories.objects.all()
-    productList = Product.objects.all()  # Fetch all products
+    productList = Product.objects.all()  
+    offerPosterList = offer.objects.all()
+    # Fetch all products
     return render(request, 'Dashboard/index.html', {
         'navCategories': categoryList,
-        'products': productList  # Pass products to the template
+        'products': productList,
+        'offers': offerPosterList
+         # Pass products to the template
     })
 
 def contact(request):
