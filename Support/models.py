a177125f04da1e01bdb7c5a96162d4ab88419edd
Apoorvs23 <Apoorv.Shamma@gmail.com>
2024-07-11 23:59:38 -0400

    updating query model...

--- Support/models.py
+++ Support/models.py
@@ -4,19 +4,20 @@ from django.db import models
 from django.conf import settings
 
 class Query(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField()
     query_type_choices = [
         ('order_status', 'Order Status'),
         ('order_delivery', 'Order Delivery'),
         ('other', 'Other'),
     ]
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
     type = models.CharField(max_length=20, choices=query_type_choices)
     description = models.TextField()
     attachment = models.FileField(upload_to='query_attachments/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    ticket_number = models.IntegerField(unique=True, editable=False)
     def __str__(self):
         return f"{self.first_name} {self.last_name} - {self.type}"
 
