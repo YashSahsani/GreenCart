2c3f1a2ba7e99b453d23296a3148d9dfdb4a9a51
Apoorvs23 <Apoorv.Shamma@gmail.com>
2024-07-12 09:53:04 -0400

    updating Query Model Form

--- Support/forms.py
+++ Support/forms.py
@@ -7,3 +7,11 @@ class QueryForm(forms.ModelForm):
     class Meta:
         model = Query
         fields = ['first_name', 'last_name', 'email', 'type', 'description', 'attachment']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'First Name'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Last Name'}),
+            'email': forms.EmailInput(attrs={'class': 'form-input', 'placeholder': 'Email'}),
+            'type': forms.Select(attrs={'class': 'form-dropdown'}),
+            'description': forms.Textarea(attrs={'class': 'form-textarea', 'placeholder': 'Description', 'rows': 5}),
+            'attachment': forms.ClearableFileInput(attrs={'class': 'form-file'}),
+        }
