f4742c4e4e6427da546ed85043eacfeb798273e9
aryan300 <aryanbatra10@gmail.com>
2024-07-14 17:41:02 -0400

    payment order_summary created

--- payments/urls.py
+++ payments/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from payments.views import payment_view, payment_success, payment_failed
+from payments.views import payment_view, payment_success, payment_failed, order_summary
 
 app_name='payments'
 urlpatterns = [
-    path('payment/', payment_view, name='payment_form'),
+    # path('payment/', payment_view, name='payment_form'),
+    path('payment/', order_summary, name='payment_form'),
     path('success/', payment_success, name='payment_success'),
     path('failed/', payment_failed, name='payment_failed'),
 ]
