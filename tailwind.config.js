ff08fde9b35ac924fe08735423a7679c82301103
YashSahsani <yashsahsani5@gmail.com>
2024-07-22 20:08:39 -0400

    fix: created new users in data.json

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-// postcss.config.js
-module.exports = {
-    plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-        // Add other PostCSS plugins here as needed
-    ]
-}
